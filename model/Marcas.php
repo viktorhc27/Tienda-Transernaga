c3652baaac0c899f9671e9339d66aa73ecb258a3
Uasa <johans.vasquez@outlook.com>
2021-10-03 14:52:29 -0300

    avance de las clases

---
+++ model/Marcas.php
@@ -0,0 +1,114 @@
+<?php
+class Marcas{
+    private $mar_id, $mar_nombre, $create_time, $update_time;
+
+    //GET Y SET
+
+    /**
+     * Metodo que permite obtener el valor de un atributo de la clase
+     * 
+     * @param       $key El atributo del que se desea obtener el valor.
+     * 
+     * @return      Se devuelve el valor del atributo.
+     */
+
+    public function __get($key){
+        return $this->$key;
+    }
+
+    public function __set($key, $value){
+        return $this->$key = $value;
+    }
+
+    public function agregar()
+    {
+        try {
+            $con = (new Conexion())->Conectar();
+            $sql = $con->prepare("insert into Marcas ("
+                . "mar_id,"
+                . "mar_nombre, "
+                . "mar_imagen, "
+                . "create_time, update_time)"
+                . "values("
+                . ":id, "
+                . ":nombre, "
+                . ":imagen, "
+                . ":create_time, update_time");
+       $sql->bindParam("mar_id",$this->ro_id);
+       $sql->bindParam("mar_nombre",$this->rol_nombre);
+       $sql->bindParam("mar_imagen",$this->rol_nombre);
+       $sql->bindParam("create_time",$this->create_time);
+       $sql->bindParam("update_time",$this->create_time);
+            $res = $sql->execute();
+            return $res;
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+    public function modificar(){
+        try{
+            $con =(new Conexion())->Conectar();
+            $sql= $con->prepare("update Marcas set mar_id=:id, mar_nombre=:nombre,mar_imagen=:imagen, create_time=:create_time, update_time=:update_time,");
+           
+            $sql->bindparam("id",$this->mar_id);
+            $sql->bindparam("nombre",$this->mar_nombre);
+            $sql->bindparam("nombre",$this->mar_imagen);
+            $sql->bindparam("create_time",$this->create_time);
+            $sql->bindparam("update_time",$this->update_time);
+
+            $res = $sql->execute();
+            if ($sql->rowCount() == 1) {
+
+                return $res;
+            } else {
+                return $res;
+            }
+
+        }catch (PDOException $e){
+            return $e->getMessage();
+        }
+    }
+
+    public function buscar(){
+        try {
+            $con = (new Conexion())->Conectar();
+            $sql = $con->prepare("SELECT * FROM Marcas WHERE mar_id = :id");
+            $sql->bindParam(':id', $id);
+            $sql->execute();
+            $res = $sql->fetch();
+            return $res;
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+
+    }
+    public function leer(){
+
+        try {
+            $con = (new Conexion())->Conectar();
+            $sql = $con->prepare("select * from Marcas");
+            $sql->execute();
+            $res = $sql->fetchAll();
+            return $res;
+        } catch (PDOException $ex) {
+            return $ex->getMessage();
+        }
+    }
+    public function Login(){
+
+        try {
+            $con = (new Conexion())->Conectar();
+            $sql = $con->prepare("SELECT * FROM Roles where mar_id = :id");
+            $sql->bindParam(':id', $this->ro_id);
+            $sql->execute();
+            $res = $sql->fetch();
+
+            return $res;
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+    public function cerrar(){}
+
+}
+?>
---
+++ model/Roles.php
@@ -0,0 +1,111 @@
+<?php
+class Roles{
+    private $ro_id, $rol_nombre,$create_time, $update_time;
+
+    //GET Y SET
+
+    /**
+     * Metodo que permite obtener el valor de un atributo de la clase
+     * 
+     * @param       $key El atributo del que se desea obtener el valor.
+     * 
+     * @return      Se devuelve el valor del atributo.
+     */
+
+    public function __get($key){
+        return $this->$key;
+    }
+
+    public function __set($key, $value){
+        return $this->$key = $value;
+    }
+
+    public function agregar()
+    {
+        try {
+            $con = (new Conexion())->Conectar();
+            $sql = $con->prepare("insert into usuarios ("
+                . "us_id,"
+                . "us_nombre, "
+                . "create_time, update_time"
+                . "roles_ro_id)"
+                . "values("
+                . ":id, "
+                . ":nombre, "
+                . ":create_time, update_time");
+       $sql->bindParam("ro_id",$this->ro_id);
+       $sql->bindParam("rol_nombre",$this->rol_nombre);
+       $sql->bindParam("create_time",$this->create_time);
+       $sql->bindParam("update_time",$this->create_time);
+            $res = $sql->execute();
+            return $res;
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+    public function modificar(){
+        try{
+            $con =(new Conexion())->Conectar();
+            $sql= $con->prepare("update Roles set ro_id=:id, rol_nombre=:nombre, create_time=:create_time, update_time=:update_time,");
+           
+            $sql->bindparam("id",$this->ro_id);
+            $sql->bindparam("nombre",$this->rol_nombre);
+            $sql->bindparam("create_time",$this->create_time);
+            $sql->bindparam("update_time",$this->update_time);
+
+            $res = $sql->execute();
+            if ($sql->rowCount() == 1) {
+
+                return $res;
+            } else {
+                return $res;
+            }
+
+        }catch (PDOException $e){
+            return $e->getMessage();
+        }
+    }
+
+    public function buscar(){
+        try {
+            $con = (new Conexion())->Conectar();
+            $sql = $con->prepare("SELECT * FROM Roles WHERE ro_id = :id");
+            $sql->bindParam(':id', $id);
+            $sql->execute();
+            $res = $sql->fetch();
+            return $res;
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+
+    }
+    public function leer(){
+
+        try {
+            $con = (new Conexion())->Conectar();
+            $sql = $con->prepare("select * from Roles");
+            $sql->execute();
+            $res = $sql->fetchAll();
+            return $res;
+        } catch (PDOException $ex) {
+            return $ex->getMessage();
+        }
+    }
+    public function Login(){
+
+        try {
+            $con = (new Conexion())->Conectar();
+            $sql = $con->prepare("SELECT * FROM Roles where ro_id = :id");
+            $sql->bindParam(':id', $this->ro_id);
+            $sql->execute();
+            $res = $sql->fetch();
+
+            return $res;
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+    public function cerrar(){}
+
+}
+?>
