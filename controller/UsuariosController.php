fb15805cc0ef9d41cca680f8f2874367a573c1db
viktorhc27 <victor.huanca1999@gmail.com>
2021-10-03 21:26:02 -0300

    registro deusuarios y admin

--- controller/UsuariosController.php
+++ controller/UsuariosController.php
@@ -9,8 +9,8 @@ switch ($accion) {
         session_start();
         $usuarios = new Usuarios();
 
-        $usuarios->__set('correo', $_REQUEST['correo']);
-        $usuarios->__set('password',$_REQUEST['password']);
+        $usuarios->__set('us_correo', $_REQUEST['correo']);
+        $usuarios->__set('us_password',$_REQUEST['password']);
 
         $res= $usuarios->login();
 
@@ -22,9 +22,37 @@ switch ($accion) {
 
         break;
 
-    case 'register':
+    case 'register_user':
+        $password_hash = password_hash($_REQUEST['password'], PASSWORD_BCRYPT);
         $usuarios = new Usuarios();
+        $usuarios->__set('us_nombre',$_REQUEST['nombre']);
+        $usuarios->__set('us_apellApp',$_REQUEST['app']);
+        $usuarios->__set('us_apellApm',$_REQUEST['apm']);
+        $usuarios->__set('us_correo',$_REQUEST['correo']);
+        $usuarios->__set('us_password',$password_hash);
+        $usuarios->__set('us_telefono',$_REQUEST['telefono']);
+        $usuarios->__set('us_direccion',$_REQUEST['direccion']);
+        $usuarios->__set('us_sexo',$_REQUEST['sexo']);
+        $usuarios->__set('roles_ro_id','usuario');
+        $usuarios->__set('create_time',date("Y-m-d H:i:s"));
+        $usuarios->__set('update_time',date("Y-m-d H:i:s"));
+        $usuarios->agregar();
+    break;
 
-        
+    case 'register_employees':
+        $password_hash = password_hash($_REQUEST['password'], PASSWORD_BCRYPT);
+        $usuarios = new Usuarios();
+        $usuarios->__set('us_nombre',$_REQUEST['nombre']);
+        $usuarios->__set('us_apellApp',$_REQUEST['app']);
+        $usuarios->__set('us_apellApm',$_REQUEST['apm']);
+        $usuarios->__set('us_correo',$_REQUEST['correo']);
+        $usuarios->__set('us_password',$password_hash);
+        $usuarios->__set('us_telefono',$_REQUEST['telefono']);
+        $usuarios->__set('us_direccion',$_REQUEST['direccion']);
+        $usuarios->__set('us_sexo',$_REQUEST['sexo']);
+        $usuarios->__set('roles_ro_id','admin');
+        $usuarios->__set('create_time',date("Y-m-d H:i:s"));
+        $usuarios->__set('update_time',date("Y-m-d H:i:s"));
+        $usuarios->agregar();
     break;
 }
