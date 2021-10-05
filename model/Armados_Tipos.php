a3d7ebc371f02acffebed2e7f9e7d9969ab32efd
Uasa <johans.vasquez@outlook.com>
2021-10-05 20:09:59 -0300

    tabla lista ahora creare la base de datos

--- model/Armados_Tipos.php
+++ model/Armados_Tipos.php
@@ -1,18 +1,99 @@
 <?php 
-Class Armados_Tipos{
+Class Armados_Tipos {
+    private $cat_id, $cat_nombre, $cat_estado, $create_time, $update_time;
+    
+    public function __get($key){
+        return $this->$key;
+    }
+
+    public function __set($key, $value){
+        return $this->$key = $value;
+    }
 
 
+    public function agregar()
+    {
+        try {
+
+            $con = (new Conexion())->Conectar();
+            $sql = $con->prepare("insert into categorias (arm_id, arm_nombre, create_time, update_time) values(:id,:nombre,:create_time,:update_time)");
+            $sql->bindParam(':id', $this->arm_id);
+            $sql->bindParam(':nombre', $this->arm_nombre);
+            $sql->bindParam(':create_time', $this->create_time);
+            $sql->bindParam(':update_time', $this->update_time);
+
+            $res = $sql->execute();
+            return $res;
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function modificar(){
+        try{
+            $con =(new Conexion())->Conectar();
+            $sql= $con->prepare("update productos set arm_id=:id, arm_nombre =:nombre,create_time=:create_time, update_time=:update_time  WHERE arm_id = :id");
+            
+            $sql->bindParam(':id', $this->cat_id);
+            $sql->bindParam(':nombre', $this->cat_nombre);
+            $sql->bindParam(':estado', $this->cat_estado);
+            $sql->bindParam(':create_time', $this->create_time);
+            $sql->bindParam(':update_time', $this->update_time);
+
+            $res = $sql->execute();
+            if ($sql->rowCount() == 1) {
+
+                return $res;
+            } else {
+                return $res;
+            }
+
+        }catch (PDOException $e){
+            return $e->getMessage();
+        }
+    }
 
 
+    public function listar()
+    {
+        try {
+            $con = (new Conexion())->Conectar();
+            $sql = $con->prepare("select * from Armados_Tipos");
+            $sql->execute();
+            $res = $sql->fetchAll();
+            return $res;
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+    public function eliminar($id)
+    {
 
-
-
-
-
-
-
+        try {
+            $con = (new Conexion())->Conectar();
+            $sql = $con->prepare("DELETE FROM Armados_Tipos WHERE arm_id = :id");
+            $sql->bindParam(":id", $id);
+            $res = $sql->execute();
+            return $res;
+        } catch (PDOException $ex) {
+            return $ex->getMessage();
+        }
+    }
+    public function buscar($id){
+        try{
+            $con = (new Conexion())->Conectar();
+            $sql = $con->prepare("select * from Armados_Tipos where  arm_id = :id");
+            $sql->bindparam(':id',$id);
+            $sql->execute();
+            $res= $sql->fetch();
+            return $res;
+        }catch (PDOException $ex){
+            return $ex->getMessage();
+        }
+    }
 
 
 
 }
+
 ?>
